d29e75bc69f4666f9205f0603d36eedee8da1483
bigsuperyu <bigsuperangel@gmail.com>
2016-05-31 12:41:47 +0800

    readme.md

---
+++ src/main/java/GuavaDemo.java
@@ -0,0 +1,14 @@
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Created by linyu on 2016/5/31.
+ */
+public class GuavaDemo {
+    public static void main(String[] args) {
+        List list = Lists.newArrayList();
+        list.add("test");
+        System.out.println(list);
+    }
+}
